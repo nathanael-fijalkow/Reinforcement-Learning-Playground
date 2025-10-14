4563b497a4b82716df0f3cdb9a52b3305d59a2a4
= <=>
2025-10-14 10:19:00 +0200

    Implémentation MonteCarlo

--- src/monte_carlo.py
+++ src/monte_carlo.py
@@ -3,16 +3,35 @@ from collections import deque
 from src.base_agent import BaseAgent
 
 class MonteCarlo(BaseAgent):
-    def __init__(self, 
-                 state_dim, 
-                 action_dim):
+    
+    def __init__ (self, 
+            state_dim, 
+            action_dim, 
+            learning_rate=0.1, 
+            gamma=0.99, 
+            epsilon=1.0, 
+            epsilon_decay=0.9994, 
+            epsilon_min=0.01):
+        
         self.q_table = np.zeros((state_dim, action_dim))
+        self.lr = learning_rate
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.epsilon_decay = epsilon_decay
+        self.epsilon_min = epsilon_min
+        self.action_dim = action_dim
 
     def select_action(self, state, greedy=False):
-        pass            
+        if greedy or np.random.rand() > self.epsilon:
+            return np.argmax(self.q_table[state])
+        return np.random.choice(self.action_dim)
 
-    def learn(self, state, action, reward, next_state, done):
-        pass
+    def learn(self, trajectory):
+        G = 0
+        for state, action, reward in reversed(trajectory):
+            G = reward + self.gamma * G
+            self.q_table[state, action] += self.lr * (G - self.q_table[state, action])
+        self.epsilon = max(self.epsilon_min, self.epsilon * self.epsilon_decay)
 
     def save(self, path):
         np.save(path, self.q_table)
@@ -31,25 +50,25 @@ def train(env, state_dim, action_dim, num_episodes, max_steps_per_episode, targe
     for episode in range(1, num_episodes + 1):
         state, _ = env.reset()
         episode_reward = 0
-        
+        trajectory=[]
         for step in range(max_steps_per_episode):
             action = agent.select_action(state)
             next_state, reward, terminated, truncated, _ = env.step(action)
             done = terminated or truncated
-
-            agent.learn(state, action, reward, next_state, done)
+            
+            trajectory.append((state,action,reward))
 
             state = next_state
             episode_reward += reward
 
             if done:
                 break
-
+        agent.learn(trajectory)
         scores_deque.append(episode_reward)
         scores.append(episode_reward)
 
         if episode % (num_episodes / 10) == 0:
-            print(f"Episode {episode}	Average Score: {np.mean(scores_deque):.2f}")
+            print(f"Episode {episode} Average Score: {np.mean(scores_deque):.2f}, Epsilon: {agent.epsilon:.2f}")
         
         if np.mean(scores_deque) >= target_score:
             print(f"Environment solved in {episode} episodes! Average Score: {np.mean(scores_deque):.2f}")
