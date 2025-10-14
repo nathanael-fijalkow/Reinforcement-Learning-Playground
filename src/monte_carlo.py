7a1b67cb47a7de2500a8cb0a09ea1c2360239a22
anas maillal <anas.maillal@gmail.com>
2025-10-15 00:17:43 +0200

    Implémentation Montée-Carlos

--- src/monte_carlo.py
+++ src/monte_carlo.py
@@ -3,16 +3,43 @@ from collections import deque
 from src.base_agent import BaseAgent
 
 class MonteCarlo(BaseAgent):
-    def __init__(self, 
-                 state_dim, 
-                 action_dim):
+    def __init__(self,
+                state_dim,
+                action_dim,
+                learning_rate=0.1,
+                epsilon=1.0,
+                epsilon_decay=0.995,
+                epsilon_min=0.01,
+                gamma=0.99):
         self.q_table = np.zeros((state_dim, action_dim))
+        self.lr = learning_rate
+        self.lr_decay = 0.995
+        self.lr_min = 0.001
+        self.epsilon = epsilon
+        self.epsilon_decay = epsilon_decay
+        self.epsilon_min = epsilon_min
+        self.action_dim = action_dim
+        self.gamma = gamma
 
     def select_action(self, state, greedy=False):
-        pass            
+        if not greedy and np.random.rand() <= self.epsilon:
+            return np.random.choice(self.action_dim)
+        else:
+            return np.argmax(self.q_table[state, :])
 
-    def learn(self, state, action, reward, next_state, done):
-        pass
+    def learn(self, trajectory):
+        G = 0
+        visited_couples = set()
+        for step in range(len(trajectory)):
+            state, action, reward = trajectory[len(trajectory) - step - 1]
+            G = reward + self.gamma * G
+            if (state, action) not in visited_couples:
+                visited_couples.add((state, action))
+                old_value = self.q_table[state, action]
+                self.q_table[state, action] = old_value + self.lr * (G - old_value)
+
+        self.epsilon = max(self.epsilon_min, self.epsilon * self.epsilon_decay)
+        self.lr = max(self.lr_min, self.lr * self.lr_decay)
 
     def save(self, path):
         np.save(path, self.q_table)
@@ -31,20 +58,19 @@ def train(env, state_dim, action_dim, num_episodes, max_steps_per_episode, targe
     for episode in range(1, num_episodes + 1):
         state, _ = env.reset()
         episode_reward = 0
-        
+        trajectory = []
         for step in range(max_steps_per_episode):
             action = agent.select_action(state)
             next_state, reward, terminated, truncated, _ = env.step(action)
             done = terminated or truncated
-
-            agent.learn(state, action, reward, next_state, done)
-
+            trajectory.append((state, action, reward))
             state = next_state
             episode_reward += reward
 
             if done:
                 break
 
+        agent.learn(trajectory)
         scores_deque.append(episode_reward)
         scores.append(episode_reward)
 
@@ -58,4 +84,4 @@ def train(env, state_dim, action_dim, num_episodes, max_steps_per_episode, targe
     
     print("\nTraining complete.")
     
-    return agent, scores
+    return agent,scores
